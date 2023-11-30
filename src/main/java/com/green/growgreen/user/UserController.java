f15d3471e2899c2e13b41bc26e650b43ef611223
firstt <khi05040@naver.com>
2023-11-30 19:37:59 +0900

    23-11-30 growgreen 보충수업

---
+++ src/main/java/com/green/growgreen/user/UserController.java
@@ -0,0 +1,16 @@
+package com.green.growgreen.user;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+    private final UserService service;
+}
---
+++ src/main/java/com/green/growgreen/user/model/UserInsDto.java
@@ -0,0 +1,12 @@
+package com.green.growgreen.user.model;
+
+
+import lombok.Data;
+
+@Data
+public class UserInsDto {
+    private int iuser;
+    private String nm;
+    private String uid;
+    private String upw;
+}
