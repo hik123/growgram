e5af8c0fc98c3a1cef34866e5f191df8bb23109b
firstt <khi05040@naver.com>
2023-11-30 20:23:09 +0900

    23-11-30 growgreen 보충수업

--- src/main/java/com/green/growgreen/user/UserController.java
+++ src/main/java/com/green/growgreen/user/UserController.java
@@ -1,8 +1,12 @@
 package com.green.growgreen.user;
 
 
+import com.green.growgreen.ResVo;
+import com.green.growgreen.user.model.UserInsDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserController {
     private final UserService service;
+
+    @PostMapping("/signup")
+    public ResVo signup(@RequestBody UserInsDto dto) {
+        return service.signup(dto);
+    }
 }
--- src/main/java/com/green/growgreen/user/model/UserInsDto.java
+++ src/main/java/com/green/growgreen/user/model/UserInsDto.java
@@ -1,11 +1,13 @@
 package com.green.growgreen.user.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
 public class UserInsDto {
-    private int iuser;
+    @JsonIgnore //swagger에서 안타나게 해줌
+    private Integer iuser;
     private String nm;
     private String uid;
     private String upw;
